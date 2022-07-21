624c7760ad56f43d00b73018d27dae39c4b14e07
Gabridg <gabri99.del@gmail.com>
2022-07-21 15:35:48 +0200

    clear components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorldVue from './components/HelloWorld.vue';
+
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header: Title,
+    Main: HelloWorldVue,
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  font-family: Helvetica, Arial, sans-serif;
   margin-top: 60px;
 }
 </style>
