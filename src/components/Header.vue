a0be18630d8b2f2ad8952f85cc3dc5094d176663
Gabridg <gabri99.del@gmail.com>
2022-07-21 15:41:04 +0200

    added Header.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="container">
+        <h1>La mia prima app con Vue CLI!</h1>
+    </div>
+</template>
+
+<script>
+import { title } from 'process';
+
+export default {
+    name: "Title",
+}
+</script>
+
+<style scoped>
+.container {
+    max-width: 1200px;
+    margin: 0 auto;
+}
+
+.container h1 {
+    color: darkblue;
+    font-weight: bold;
+    text-shadow: 5px #000;
+}
+</style>
